3d0fea3863d426a0cecf44ff2128c3ea941ee47f
RadostinStoyanov <Radostin_Stoyanov@students.softuni.bg>
2024-07-30 19:39:59 +0300

    creating of rest api for offers

---
+++ src/test/java/bg/softuni/farmers_market/offers/OffersApplicationTests.java
@@ -0,0 +1,13 @@
+package bg.softuni.farmers_market.offers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OffersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
